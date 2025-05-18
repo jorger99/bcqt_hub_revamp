777bd6be92319060b54e73e1ffe2fc159737689b
jorger99 <jorge.ramirez@colorado.edu>
2025-05-18 15:54:38 -0600

    first set of changes to power_cycle_HEMT.py script

--- bcqthub/scripts/power_cycle_HEMTs.py
+++ bcqthub/scripts/power_cycle_HEMTs.py
@@ -13,13 +13,15 @@ cfg = {
 
 ctrl = HEMTController(cfg, debug=False)
 
+sweep_turn_on = [1.1, 0.7, 0.02]  # [gate stop, drain stop, step]
+
 log.info("Soft-starting HEMTs")
 gate_iv_on, drain_iv_on = ctrl.turn_on(
-    gate_stop=1.1, drain_stop=0.7,   # volts, volts
-    step=0.02, delay=0.2,            # volts, seconds
-    dry_run=False,
+    *sweep_turn_on, delay=0.5,     # if dry_run = True, everything 
+    dry_run=True,                  # runs without sending cmds to instrument
 )
 
+log.info("Plotting IV curves for turn_on()")
 plot_iv_pair(gate_iv_on, drain_iv_on)
 
 # Now show a single comprehensive dump of the instrument state
@@ -27,12 +29,16 @@ plot_iv_pair(gate_iv_on, drain_iv_on)
 
 log.info("Soft-shutting down HEMTs")
 gate_iv_off, drain_iv_off = ctrl.turn_off(
-    step=0.02, delay=0.2,   # volts, seconds
-    dry_run=False,          
+    step=0.02, delay=0.2,          # takes whatever the input is, ramps to zero
+    dry_run=True,                  # runs without sending cmds to instrument
 )
 
-log.info("Done.")
+log.info("Plotting IV curves for turn_off()")
 plot_iv_pair(gate_iv_off, drain_iv_off)
+
+
+log.info("Done.")
+
     
 
 
